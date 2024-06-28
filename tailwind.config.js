8b8a2122132f510d1e61f0679f5bc622535eb57b
Andrew Lara <aandrewlaraa@gmail.com>
2024-06-28 01:07:45 -0400

    using next.js components for quicker development

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust this line if your file paths are different
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
