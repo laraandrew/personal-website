4565d57f527e8404b01e6f5d16c67e6d18fdca38
Andrew Lara <aandrewlaraa@gmail.com>
2024-07-09 13:49:56 -0400

    improved organization of homepage and its components/sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,18 @@
-import WelcomeSection from '../components/WelcomeSection';
-import AboutSection from '../components/AboutSection';
-import ContactForm from '../components/ContactForm';
-import ProjectCard from '../components/ProjectCard';
+import WelcomeSection from '../components/Home-Sections/WelcomeSection';
+import AboutSection from '../components/Home-Sections/AboutSection';
+import ContactForm from '../components/Home-Sections/ContactForm';
+import ProjectSection from '@/components/Home-Sections/ProjectSection';
 
 export default function Page() {
   return (
     <>
       <WelcomeSection />
       <main>
-        <section id="projects" className="main-section flex justify-evenly items-center flex-col gap-4">
-          <header className="">Projects</header>
-          <ProjectCard
-            title="LensByLara"
-            description="A simple hobby that turned into a passion"
-            imgSrc="/assets/imgs/photography.png"
-            buttonText="Learn more!"
-          />
-          <ProjectCard
-            title="Politics and Progress"
-            description="Proin tortor enim, hendrerit vel laoreet ut, pulvinar quis velit. Suspendisse vitae maximus lorem, ac hendrerit massa"
-            imgSrc="/assets/imgs/government.png"
-            buttonText="Dive in"
-          />
-          <ProjectCard
-            title="Computer Science"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nisi neque, faucibus ut eros in, tristique venenatis dui."
-            imgSrc="/assets/imgs/technology.png"
-            buttonText="Click here"
-          />
-        </section>
+
+        <ProjectSection />
         <AboutSection />
         <ContactForm />
+
       </main>
     </>
   );
