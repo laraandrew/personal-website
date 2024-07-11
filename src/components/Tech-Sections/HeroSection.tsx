a6dbfb902e723bb1c6589fc7690fbc337e1bfafc
Andrew Lara <aandrewlaraa@gmail.com>
2024-07-10 21:21:37 -0400

    finally functional /technology page. need to fix routing from home to it, then photography and gov

--- src/components/Tech-Sections/HeroSection.tsx
+++ src/components/Tech-Sections/HeroSection.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="flex flex-col justify-center items-center h-screen bg-cover bg-center text-white text-center" style={{ backgroundImage: "url('/assets/imgs/placeholder-hero.png')" }}>
-      <h1 className="text-5xl font-bold mb-4">Computer Science</h1>
-      <p className="text-2xl">Embrace change, my Journey with Technology</p>
+    <section className="flex flex-col justify-center items-center h-screen bg-cover bg-center text-white text-center" style={{ backgroundImage: "url('assets/imgs/CS_hero.png')" }}>
+      <h1 className="text-5xl font-bold mb-4">My Journey with Technology</h1>
     </section>
   );
 }
